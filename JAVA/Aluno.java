bd2d0f31d9752394466cd4f8b4cdfc0d4f1a867a
Arthur Araujo <arthuraraujo@sempreceub.com>
2025-06-04 21:04:30 -0300

    Add files via upload

---
+++ JAVA/Aluno.java
@@ -0,0 +1,15 @@
+// Subclasse Aluno
+public class Aluno extends Pessoa {
+    private String curso;
+
+    public Aluno(String nome, int idade, String curso) {
+        super(nome, idade);
+        this.curso = curso;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Curso: " + curso);
+    }
+}
